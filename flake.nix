ad7b38f2cc2b8947a1d9bd62df159c5bdb30a655
Chua Hou <human+github@chuahou.dev>
2021-08-01 21:04:53 +0800

    flake: add MahjongRepository/mahjong

--- flake.nix
+++ flake.nix
@@ -1,20 +1,33 @@
 {
-  inputs.nixpkgs.url  = "nixpkgs/nixos-21.05";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.05";
+    py-mahjong = { url = "github:MahjongRepository/mahjong/v1.1.11"; flake = false; };
+  };
 
   outputs = inputs@{ self, nixpkgs, ... }:
   let
     system = "x86_64-linux";
     pkgs   = nixpkgs.legacyPackages.${system};
 
-  in rec {
-    defaultPackage.${system} =
-      pkgs.haskellPackages.callCabal2nix "fufu-api" ./. {};
+    hspkg = pkgs.haskellPackages.callCabal2nix "fufu-api" ./. {};
 
-    devShell.${system} =
-      (pkgs.haskell.lib.overrideCabal defaultPackage.${system} (old: {
-        buildTools = (old.buildTools or []) ++ (with pkgs.haskellPackages; [
+  in {
+    defaultPackage.${system} =
+      hspkg.overrideAttrs (old: {
+
+        buildInputs = (old.buildInputs or []) ++ (with pkgs; [
+          (python3Full.withPackages (pp: with pp; [
+            (pp.buildPythonPackage {
+              pname   = "mahjong";
+              version = "v1.1.11";
+              src     = inputs.py-mahjong;
+            })
+          ]))
+        ]);
+
+        nativeBuildInputs = (old.nativeBuildInputs or []) ++ (with pkgs; [
           cabal-install
         ]);
-      })).env;
+      });
   };
 }
